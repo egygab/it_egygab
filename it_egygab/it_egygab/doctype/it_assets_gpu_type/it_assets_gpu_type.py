e1625815510796a5c8047a5734804a49e393c476
Mahmoud <m.naguib@egygab.com>
2025-12-15 15:50:44 +0200

    Change asset types

---
+++ it_egygab/it_egygab/doctype/it_assets_gpu_type/it_assets_gpu_type.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Mahmoud and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ITAssetsGPUType(Document):
+	pass
